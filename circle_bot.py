194b83ff4a828dc3f4e224581a66b7e34f73f025
Yron Lance Talban <talbanl306@gmail.com>
2026-02-16 20:26:18 -0500

    Fix cv2 PyInstaller bundling with --collect-submodules

--- circle_bot.py
+++ circle_bot.py
@@ -20,7 +20,6 @@ Dependencies:
   pip install opencv-python numpy mss keyboard
 """
 
-import cv2.load_config_py3  # noqa: F401 – force PyInstaller to bundle this
 import cv2
 import numpy as np
 import mss
